4c849c75a6f6f7a8d92f79eaae4d54b3e363fcce
Adrian Mieńkowski <amienkowski@gmail.com>
2025-07-08 07:18:39 +0200

    config for render.com deployment

--- src/app/service/jwt.interceptor.ts
+++ src/app/service/jwt.interceptor.ts
@@ -7,6 +7,7 @@ import {catchError, Observable, switchMap, throwError} from 'rxjs';
 import {inject} from '@angular/core';
 import {AuthService} from './auth.service';
 import {AuthResponse} from '../model/auth-response.model';
+import {Router} from '@angular/router';
 
 function sendRequestWithToken(req: HttpRequest<any>, next: HttpHandlerFn, token: string | null): Observable<HttpEvent<any>> {
   if (token && !req.url.includes('/auth/refresh')) {
@@ -20,28 +21,28 @@ function sendRequestWithToken(req: HttpRequest<any>, next: HttpHandlerFn, token:
 export const jwtInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> => {
   const authService: AuthService = inject(AuthService);
   const token: string | null = localStorage.getItem('jwt');
-  console.log("Token z local storage: " + token);
+  const router: Router = inject(Router);
   return sendRequestWithToken(req, next, token).pipe(
     catchError((error: HttpErrorResponse) => {
-      console.log("Interceptor caught an error:", error);
       if (error.status === 401 && error.error?.error === 'TOKEN_EXPIRED') {
-        console.log("ERROR: " + error.status + ", " + error.error?.error)
         return authService.refreshToken().pipe(
           switchMap((response: AuthResponse) => {
-            console.log("Tokens refreshed. JWT: " + response.jwt);
             localStorage.setItem('jwt', response.jwt);
             return sendRequestWithToken(req, next, response.jwt);
           }),
           catchError((err) => {
-            console.log("Error while refreshing tokens.");
             authService.clearTokens();
-            return throwError(() => err);
+            router.navigate(['/login-page']);
+            return throwError((): HttpErrorResponse => err);
           })
         );
+      } else if (error.status === 401) {
+        authService.clearTokens();
+        router.navigate(['/login-page']);
+        return throwError((): HttpErrorResponse => error);
       } else {
-        console.log("NIE ERROR: " + error.status + ", " + error.error?.error)
+        return throwError((): HttpErrorResponse => error);
       }
-      return throwError((): HttpErrorResponse => error);
     })
   );
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://hms-backend-hpc6.onrender.com/hms/v1'
+};
